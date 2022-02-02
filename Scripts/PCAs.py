a814968dbf9c55bde5e595607c73cddf8f9c013a
edhirst <edward.hirst@city.ac.uk>
2022-02-02 13:41:55 +0000

    add PCA eigenvalue analysis

--- Scripts/PCAs.py
+++ Scripts/PCAs.py
@@ -8,7 +8,7 @@ from sklearn.decomposition import PCA
 #%% #Import data
 #Non-palindromic & Palindromic HS coefficients for orders: 0->9 (low) & 1000->1009 (high)
 PalinHS_low, PalinHS_high, NonPalinHS_low, NonPalinHS_high = [], [], [], []
-with open('../Data/BC-PalindromicHS.txt','r') as PalinData: #...confirm correct file path, used Palindromic data has 6 repeats of 10,000 HS generated (i.e. dataset ~ balanced)
+with open('./HilbertSeriesML/Data/BC-PalindromicHS.txt','r') as PalinData: #...confirm correct file path, used Palindromic data has 6 repeats of 10,000 HS generated (i.e. dataset ~ balanced)
     linecounter = 0    
     for line in PalinData:
         linecounter += 1
@@ -21,7 +21,7 @@ with open('../Data/BC-PalindromicHS.txt','r') as PalinData: #...confirm correct 
             continue
         PalinHS_low.append(coeffs[0])
         PalinHS_high.append(coeffs[1])
-with open('../Data/BC-NonPalindromicHS.txt','r') as NonPalinData: #...confirm correct file path
+with open('./HilbertSeriesML/Data/BC-NonPalindromicHS.txt','r') as NonPalinData: #...confirm correct file path
     linecounter = 0    
     for line in NonPalinData:
         linecounter += 1
@@ -38,26 +38,32 @@ del(PalinData,NonPalinData,line,linecounter,coeffs)
     
 #Import datafiles of fake & real HS coefficients for orders 0->99
 fake_HS, real_HS = [], []
-with open('../Data/BC-fakeHS.txt','r') as fakeHSfile: #...confirm correct file path
+with open('./HilbertSeriesML/Data/BC-fakeHS.txt','r') as fakeHSfile: #...confirm correct file path
     for line in fakeHSfile:
         fake_HS.append(literal_eval(line))
-with open('../Data/BC-realHS.txt','r') as realHSfile: #...confirm correct file path
+with open('./HilbertSeriesML/Data/BC-realHS.txt','r') as realHSfile: #...confirm correct file path
     for line in realHSfile:
         real_HS.append(literal_eval(line))
 del(fakeHSfile,realHSfile,line)
 
 #%% #Perform PCA
+#Select whether to use the high or low coefficeint data for the Gorenstein PCA (real-fake is fixed to low coefficients)
+Gorenstein_high_check = True       #...make False to consider to 0->100 coefficients, and True to consider the 1000->1009 coefficients
+if Gorenstein_high_check: Palin, NonPalin = PalinHS_high, NonPalinHS_high
+else:                     Palin, NonPalin = PalinHS_low, NonPalinHS_low
+    
 #Define the standardisers and PCA projectors
 scaler_g = StandardScaler()
-pca_G = PCA(n_components=2)
+if Gorenstein_high_check: pca_G = PCA(n_components=10)
+else:                     pca_G = PCA(n_components=101)
 scaler_rf = StandardScaler()
-pca_rf = PCA(n_components=2)
+pca_rf = PCA(n_components=100)
 
 #Perform standardisation and PCA
 #Gorenstein
-scaler_g.fit(NonPalinHS_low+PalinHS_low)
-transformed_NonPalin = scaler_g.transform(NonPalinHS_low)
-transformed_Palin = scaler_g.transform(PalinHS_low)
+scaler_g.fit(NonPalin+Palin)
+transformed_NonPalin = scaler_g.transform(NonPalin)
+transformed_Palin = scaler_g.transform(Palin)
 pca_G.fit(np.concatenate((transformed_NonPalin,transformed_Palin)))
 pca_nonpalin = pca_G.transform(transformed_NonPalin)
 pca_palin = pca_G.transform(transformed_Palin)
@@ -69,7 +75,7 @@ pca_rf.fit(np.concatenate((transformed_fake,transformed_real)))
 pca_fake = pca_rf.transform(transformed_fake)
 pca_real = pca_rf.transform(transformed_real)
 
-#%% #Plotting Grenstein 2d PCA
+#%% #Plotting Gorenstein 2d PCA
 #Gorenstein
 plt.figure('Gorenstein PCA')
 plt.scatter(pca_palin[:,0],pca_palin[:,1],label='Gorenstein',alpha=0.1,color='blue')
@@ -81,6 +87,11 @@ for lh in leg.legendHandles:
     lh.set_alpha(1)
 #plt.grid()
 #plt.savefig('./GorensteinPCA.pdf')
+
+#Output PCA information
+G_covar_matrix = pca_G.get_covariance()
+print('Explained Variance ratio: '+str(pca_G.explained_variance_ratio_)+' (i.e. normalised eigenvalues)') #...note components gives rows as eigenvectors
+print('\nDominant PCs:\n'+str(pca_G.components_[0])+'\n'+str(pca_G.components_[1]))
 
 #%% #Plotting Real/Fake 2d PCA
 #Real/Fake
@@ -95,6 +106,11 @@ for lh in leg.legendHandles:
     lh.set_alpha(1)
 #plt.grid()
 #plt.savefig('./RealFakePCA_2d.pdf')
+
+#Output PCA information
+rf_covar_matrix = pca_rf.get_covariance()
+print('Explained Variance ratio: '+str(pca_rf.explained_variance_ratio_)+' (i.e. normalised eigenvalues)') #...note components gives rows as eigenvectors
+print('\nDominant PC: '+str(pca_rf.components_[0]))
 
 #%% #Plotting Real/Fake 1d PCA
 #Real/Fake
@@ -111,4 +127,9 @@ ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
 plt.yticks([]) 
 #plt.savefig('./RealFakePCA_1d.pdf',bbox_inches='tight')
+
+#Output PCA information
+rf_covar_matrix = pca_rf.get_covariance()
+print('Explained Variance ratio: '+str(pca_rf.explained_variance_ratio_)+' (i.e. normalised eigenvalues)') #...note components gives rows as eigenvectors
+print('\nDominant PC: '+str(pca_rf.components_[0]))
 
